82864ed2a213b6f81b59a2582d5b6ea8e1b6c1d3
duckyluuk <58784017+duckyluuk@users.noreply.github.com>
2022-12-24 19:02:32 +0100

    improve day 19

--- day 19/19.py
+++ day 19/19.py
@@ -15,10 +15,10 @@ with open("19.txt") as f:
         cost.append([obs1,obs2,0,0])
         cost.append([geo1,0,geo2,0])
         costs.append(cost)
+
     
     def search(costs,states,loops):
-        checked = []
-        for i in range(loops):
+        for loop in range(loops):
             newStates = []
             for state in states:
                 robots=state["robots"]
@@ -30,18 +30,22 @@ with open("19.txt") as f:
 
                 for i in range(3,-1,-1):
                     cost = costs[i]
+
                     if all([resources[j]>=cost[j] for j in range(4)]):
+                        if robots[i] >= max(cost[i] for cost in costs) and i<3:
+                            continue # dont make more bots of a type than needed for the most expensive upgrade
+
                         finalRobots = [r for r in robots]
                         finalRobots[i]+=1
                         finalResources=[newResources[j]-cost[j] for j in range(4)]
                         newState = {"robots":finalRobots,"resources":finalResources}
                         newStates.append(newState)
-                        if i>1: break # if you can make a geode or obsidian bot then do so
-            sortKey = lambda x: (x["resources"][3]+x["robots"][3]*(loops-i))*1000 + ( # sort all states
-                                 x["resources"][2]+x["robots"][2]*(loops-i))*100  + ( # by what their final score would be
-                                 x["resources"][1]+x["robots"][1]*(loops-i))*10   + (
-                                 x["resources"][0]+x["robots"][0]*(loops-i)) 
-            states = sorted(newStates, key = sortKey)[-250:]
+                        if i>2: break # if you can make a geode bot then do so
+            sortKey = lambda x: (x["resources"][3]+x["robots"][3]*(loops-loop))*1000 + ( # sort all states
+                                 x["resources"][2]+x["robots"][2]*(loops-loop))*100  + ( # by what their final score would be
+                                 x["resources"][1]+x["robots"][1]*(loops-loop))*10   + (
+                                 x["resources"][0]+x["robots"][0]*(loops-loop)) 
+            states = sorted(newStates, key = sortKey)[-200:] # the number might need to be higher for some but this works for my input
         return max([x["resources"][3] for x in states])
             
 
@@ -51,7 +55,7 @@ with open("19.txt") as f:
     for i in range(len(costs)):
         c=costs[i]
         value = search(c,[currentState],24)
-        res += value * (i-1)
+        res += value * (i+1)
         #print(f"Blueprint {i+1} part 1: {value}")
 
     print(res)
